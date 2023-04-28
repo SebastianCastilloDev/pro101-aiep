a4fe3b2658f8421b836a232320bccf9b067d6f59
Sebastian Castillo <tantrum5535@gmail.com>
2023-04-28 19:40:00 -0400

    subida listas

--- diapo76.py
+++ diapo76.py
@@ -1,8 +1,10 @@
 # Año bisiesto
 
 anio = int(input('Ingrese el año: '))
+# 1.- Aquellos años que son divisibles entre 4, pero no entre 100, son bisiestos.
+# 2.- Sin embargo, los años divisibles entre 100 y entre 400 sí que son bisiestos.
 
-if(anio % 4 == 0 and anio % 100 != 0) or (anio % (4*100) == 0):
+if (anio % 4 == 0 and anio % 100 != 0) or (anio % (400) == 0):
     print('año bisiesto = ', anio)
 else:
-    print('El año no es bisiesto' )
+    print('El año no es bisiesto')
